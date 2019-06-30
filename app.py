9e53afba9ded1f7f04ee3defb23e3449583ea9f4
jgramsch <jigramsch@uc.cl>
2019-06-30 04:43:25 -0400

    debug

--- app.py
+++ app.py
@@ -123,6 +123,7 @@ def get_messages_location(msg_info):
     date_min = datetime.datetime.strptime(msg_info[1][2:],"%y-%m-%d")
     date_max = datetime.datetime.strptime(msg_info[2][2:],"%y-%m-%d")
     res_msgs = [x for x in all_msgs if date_min < datetime.datetime.strptime(x["date"][2:],"%y-%m-%d") and date_max > datetime.datetime.strptime(x["date"][2:],"%y-%m-%d") ]
+    print(type(json.jsonify(res_msgs)))
     return str(res_msgs)
     return manage_messages(json.jsonify(res_msgs), ["message"])
 
